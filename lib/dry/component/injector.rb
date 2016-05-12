c1e95cf4265dcd601d29a543ba8ccf96a21667e9
Tim Riley <tim@openmonkey.com>
2016-05-12 21:52:14 +1000

    Update Injector API and add unit tests

--- lib/dry/component/injector.rb
+++ lib/dry/component/injector.rb
@@ -3,53 +3,74 @@ require "dry-auto_inject"
 module Dry
   module Component
     class Injector
+      class Strategy
+        # @api private
+        attr_reader :container
+
+        # @api private
+        attr_reader :injector
+
+        # @api private
+        def initialize(container, type)
+          @container = container
+          @injector = if type == :args
+            Dry::AutoInject(container)
+          else
+            Dry::AutoInject(container).send(type)
+          end
+        end
+
+        # @api public
+        def [](*deps)
+          load_components(*deps)
+          injector[*deps]
+        end
+
+        private
+
+        def load_components(*components)
+          components = components.dup
+          aliases = components.last.is_a?(Hash) ? components.pop : {}
+
+          (components + aliases.values).each do |key|
+            container.load_component(key) unless container.key?(key)
+          end
+        end
+      end
+
+      # @api private
       attr_reader :container
-      attr_reader :type
-      attr_reader :injector
 
       # @api private
       def initialize(container)
         @container = container
       end
 
-      # @api private
-      def injectors
-        @injectors ||= Hash.new do |h, injector_type|
-          h[injector_type] = if injector_type == :args
-            Dry::AutoInject(container)
-          else
-            Dry::AutoInject(container).send(injector_type)
-          end
-        end
+      # @api public
+      def [](*deps)
+        args[*deps]
       end
 
       # @api public
-      def args(*deps)
-        load_components(*deps)
-        injectors[:args][*deps]
-      end
-      alias_method :[], :args
-
-      # @api public
-      def hash(*deps)
-        load_components(*deps)
-        injectors[:hash][*deps]
+      def args
+        strategies[:args]
       end
 
       # @api public
-      def kwargs(*deps)
-        load_components(*deps)
-        injectors[:kwargs][*deps]
+      def hash
+        strategies[:hash]
+      end
+
+      # @api public
+      def kwargs
+        strategies[:kwargs]
       end
 
       private
 
-      def load_components(*components)
-        components = components.dup
-        aliases = components.last.is_a?(Hash) ? components.pop : {}
-
-        (components + aliases.values).each do |key|
-          container.load_component(key) unless container.key?(key)
+      def strategies
+        @strategies ||= Hash.new do |h, strategy_type|
+          Strategy.new(container, strategy_type)
         end
       end
     end
