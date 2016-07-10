c6c3b0ceb42e56aa39bd1866c3ea8bfc55e40d1e
Tim Riley <tim@openmonkey.com>
2016-07-10 17:27:48 +0930

    Support all dry-auto_inject strategies from container’s injector

--- lib/dry/component/injector.rb
+++ lib/dry/component/injector.rb
@@ -2,7 +2,7 @@ require "dry-auto_inject"
 
 module Dry
   module Component
-    class Injector
+    class Injector < BasicObject
       # @api private
       attr_reader :container
 
@@ -10,14 +10,13 @@ module Dry
       attr_reader :injector
 
       # @api private
-      def initialize(container, strategy: nil, strategies_cache: nil)
+      def initialize(container, strategy: nil)
         @container = container
-        @strategies = strategies_cache
         @injector =
           if strategy
-            Dry::AutoInject(container).send(strategy)
+            ::Dry::AutoInject(container).send(strategy)
           else
-            Dry::AutoInject(container)
+            ::Dry::AutoInject(container)
           end
       end
 
@@ -27,35 +26,22 @@ module Dry
         injector[*deps]
       end
 
-      # @api public
-      def args
-        strategies[:args]
+      def method_missing(name, *args, &block)
+        ::Dry::Component::Injector.new(container, strategy: name)
       end
 
-      # @api public
-      def hash
-        strategies[:hash]
-      end
-
-      # @api public
-      def kwargs
-        strategies[:kwargs]
+      def respond_to_missing?(name, _include_private = false)
+        injector.respond_to?(name)
       end
 
       private
 
       def load_components(*components)
         components = components.dup
-        aliases = components.last.is_a?(Hash) ? components.pop : {}
+        aliases = components.last.is_a?(::Hash) ? components.pop : {}
 
         (components + aliases.values).each do |key|
           container.load_component(key) unless container.key?(key)
-        end
-      end
-
-      def strategies
-        @strategies ||= Hash.new do |cache, strategy|
-          cache[strategy] = self.class.new(container, strategy: strategy, strategies_cache: cache)
         end
       end
     end
