2c8fd88028d990b82050bffb585047fe88c7299a
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-08-10 13:38:26 +0200

    Extend spec for lazy-bootable deps

--- spec/integration/import_spec.rb
+++ spec/integration/import_spec.rb
@@ -13,22 +13,51 @@ RSpec.describe 'Lazy-booting external deps' do
           config.name = :main
         end
       end
-
-      App.import(Umbrella)
-      Import = App.injector
     end
   end
 
-  let(:user_repo) do
-    Class.new { include Test::Import['core.db.repo'] }.new
+  shared_examples_for 'lazy booted dependency' do
+    it 'lazy boots an external dep provided by top-level container' do
+      expect(user_repo.repo).to be_instance_of(Db::Repo)
+    end
+
+    it 'loads an external dep during finalization' do
+      system.finalize!
+      expect(user_repo.repo).to be_instance_of(Db::Repo)
+    end
   end
 
-  it 'lazy boots an external dep provided by top-level container' do
-    expect(user_repo.repo).to be_instance_of(Db::Repo)
+  context 'when top-lvl container provide the depedency' do
+    let(:user_repo) do
+      Class.new { include Test::Import['core.db.repo'] }.new
+    end
+
+    let(:system) { Test::App }
+
+    before do
+      module Test
+        App.import(Umbrella)
+        Import = App.injector
+      end
+    end
+
+    it_behaves_like 'lazy booted dependency'
   end
 
-  it 'loads an external dep during finalization' do
-    Test::App.finalize!
-    expect(user_repo.repo).to be_instance_of(Db::Repo)
+  context 'when top-lvl container requires the dependency from the imported container' do
+    let(:user_repo) do
+      Class.new { include Test::Import['db.repo'] }.new
+    end
+
+    let(:system) { Test::Umbrella }
+
+    before do
+      module Test
+        Umbrella.import(App)
+        Import = Umbrella.injector
+      end
+    end
+
+    it_behaves_like 'lazy booted dependency'
   end
 end
