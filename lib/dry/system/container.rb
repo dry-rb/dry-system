8922b58bc5f9a96c8ca9096e825fe93686b15ef2
Piotr Solnica <piotr.solnica+oss@gmail.com>
2016-08-13 20:54:12 +0200

    Add some YARD docs to Container [ci skip]

--- lib/dry/system/container.rb
+++ lib/dry/system/container.rb
@@ -14,6 +14,48 @@ require 'dry/system/constants'
 
 module Dry
   module System
+    # Abstract container class to inherit from
+    #
+    # Container class is treated as a global registry with all system components.
+    # Container can also import dependencies from other containers, which is useful
+    # in complex systems that are split into sub-systems.
+    #
+    # Container can be finalized, which triggers loading of all the defined components
+    # within a system, after finalization it becomes frozen. This typically happens in cases
+    # like booting a web application.
+    #
+    # Before finalization, Container can lazy-load components on demand. A component can be
+    # a simple class defined in a single file, or a complex component which has start/runtime/stop
+    # lifecycle, and it's defined in a boot file. Components which specify their dependencies using
+    # Import module can be safely required in complete isolation, and Container will resolve and load
+    # these dependencies automatically.
+    #
+    # Furthermore, Container supports auto-registering components based on dir/file naming conventions.
+    # This reduces a lot of boilerplate code as all you have to do is to put your classes under configured
+    # directories and their instances will be automatically registered within a container.
+    #
+    # Every container needs to be configured with following settings:
+    #
+    #   * `:name` - a unique container identifier
+    #   * `:root` - a system root directory (defaults to `pwd`)
+    #   * `:core_dir` - directory name relative to root, where bootable components can be defined in `boot` dir
+    #                   this defaults to `component`
+    #
+    # @example
+    #   class MyApp < Dry::System::Container
+    #     configure do |config|
+    #       config.name = :my_app
+    #     end
+    #
+    #     # this will configure $LOAD_PATH to include your `lib` dir
+    #     load_paths!('lib)
+    #
+    #     # this will auto-register classes from 'lib/components'. ie if you add `lib/components/repo.rb`
+    #     # which defines `Repo` class, then it's instance will be automatically available as `MyApp['repo']`
+    #     auto_register!('lib/components')
+    #   end
+    #
+    # @api public
     class Container
       extend Dry::Configurable
       extend Dry::Container::Mixin
@@ -29,12 +71,22 @@ module Dry
       setting :importer, Dry::System::Importer
 
       class << self
+        # Configures the container
+        #
+        # @return [self]
+        #
+        # @api public
         def configure(&block)
           super(&block)
           load_paths!(config.core_dir)
           self
         end
 
+        # Registers another container for import
+        #
+        # @param other [Hash,Dry::Container::Namespace,Dry::System::Container]
+        #
+        # @api public
         def import(other)
           case other
           when Hash then importer.register(other)
@@ -46,11 +98,29 @@ module Dry
           end
         end
 
+        # Registers finalization function for a bootable component
+        #
+        # @param name [Symbol] a unique identifier for a bootable component
+        #
+        # @see Lifecycle
+        #
+        # @return [self]
+        #
+        # @api public
         def finalize(name, &block)
           booter[name] = [self, block]
           self
         end
 
+        # Finalizes the container
+        #
+        # This triggers importing components from other containers, booting registered components
+        # and auto-registering components. It should be called only in places where you want to
+        # finalize your system as a whole, ie when booting a web application
+        #
+        # @return [self]
+        #
+        # @api public
         def finalize!(&block)
           return self if frozen?
 
@@ -63,46 +133,42 @@ module Dry
           freeze
         end
 
+        # Boots a specific component
+        #
+        # As a result, `start` and `runtime` lifecycle triggers are called
+        #
+        # @param name [Symbol] the name of a registered bootable component
+        #
+        # @return [self]
+        #
+        # @api public
         def boot!(name)
           booter.boot!(name)
           self
         end
 
+        # Boots a specific component but calls only `start` lifecycle trigger
+        #
+        # This way of booting is useful in places where a heavy dependency is
+        # needed but its runtime environment is not required
+        #
+        # @param name [Symbol] the name of a registered bootable component
+        #
+        # @return [self]
+        #
+        # @api public
         def boot(name)
           booter.boot(name)
           self
         end
 
-        def auto_register!(dir, &block)
-          auto_registrar.(dir, &block)
-          self
-        end
-
-        def component(key)
-          Component.new(
-            key,
-            loader: config.loader,
-            namespace: config.default_namespace,
-            separator: config.namespace_separator
-          )
-        end
-
-        def injector(options = {})
-          Injector.new(self, options: options)
-        end
-
-        def require(*paths)
-          paths.flat_map { |path|
-            path.to_s.include?('*') ? Dir[root.join(path)] : root.join(path)
-          }.each { |path|
-            Kernel.require path.to_s
-          }
-        end
-
-        def root
-          config.root
-        end
-
+        # Sets load paths relative to the container's root dir
+        #
+        # @param *dirs [Array<String>]
+        #
+        # @return [self]
+        #
+        # @api public
         def load_paths!(*dirs)
           dirs.map(&root.method(:join)).each do |path|
             next if load_paths.include?(path)
@@ -112,22 +178,85 @@ module Dry
           self
         end
 
+        # Auto-registers components from the provided directory
+        #
+        # The directory must be relative to the configured root path
+        #
+        # @param dir [String]
+        #
+        # @return [self]
+        #
+        # @api public
+        def auto_register!(dir, &block)
+          auto_registrar.(dir, &block)
+          self
+        end
+
+        # Builds injector for this container
+        #
+        # An injector is a useful mixin which injects dependencies into
+        # automatically defined constructor.
+        #
+        # @param options [Hash] injector options
+        #
+        # @api public
+        def injector(options = {})
+          Injector.new(self, options: options)
+        end
+
+        # Requires one or more files relative to the container's root
+        #
+        # @param *paths [Array<String>] one or more paths, supports globs too
+        #
+        # @api public
+        def require(*paths)
+          paths.flat_map { |path|
+            path.to_s.include?('*') ? Dir[root.join(path)] : root.join(path)
+          }.each { |path|
+            Kernel.require path.to_s
+          }
+        end
+
+        # Returns container's root path
+        #
+        # @return [Pathname]
+        #
+        # @api public
+        def root
+          config.root
+        end
+
+        # @api private
         def load_paths
           @load_paths ||= []
         end
 
+        # @api private
         def booter
           @booter ||= config.booter.new(root.join("#{config.core_dir}/boot"))
         end
 
+        # @api private
         def auto_registrar
           @auto_registrar ||= config.auto_registrar.new(self)
         end
 
+        # @api private
         def importer
           @importer ||= config.importer.new(self)
         end
 
+        # @api private
+        def component(key)
+          Component.new(
+            key,
+            loader: config.loader,
+            namespace: config.default_namespace,
+            separator: config.namespace_separator
+          )
+        end
+
+        # @api private
         def require_component(component)
           return if key?(component.identifier)
 
@@ -138,6 +267,7 @@ module Dry
           Kernel.require(component.path) && yield
         end
 
+        # @api private
         def load_component(key)
           return self if key?(key)
 
@@ -156,6 +286,7 @@ module Dry
 
         private
 
+        # @api private
         def load_local_component(component, fallback = false)
           if component.bootable?(booter.path) || component.file_exists?(load_paths)
             booter.boot_dependency(component) unless frozen?
@@ -170,6 +301,7 @@ module Dry
           end
         end
 
+        # @api private
         def load_external_component(component)
           container = importer[component.namespace]
           container.load_component(component.identifier)
