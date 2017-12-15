57ddcefd11242fae8a81047f36face8b5d8ea868
Piotr Solnica <piotr.solnica+oss@gmail.com>
2017-12-15 09:33:01 +0100

    Add a plugin system for container extensions

---
+++ lib/dry/system/plugins.rb
@@ -0,0 +1,60 @@
+module Dry
+  module System
+    module Plugins
+      # @api private
+      class Plugin
+        attr_reader :name
+
+        attr_reader :mod
+
+        attr_reader :block
+
+        # @api private
+        def initialize(name, mod, &block)
+          @name = name
+          @mod = mod
+          @block = block
+        end
+
+        # @api private
+        def apply_to(system, options)
+          system.extend(options.any? ? mod.new(options) : mod)
+          system.instance_eval(&block) if block
+          system
+        end
+      end
+
+      # Register a plugin
+      #
+      # @param [Symbol] name The name of a plugin
+      # @param [Class] plugin Plugin module
+      #
+      # @return [Plugins]
+      #
+      # @api public
+      def self.register(name, plugin, &block)
+        registry[name] = Plugin.new(name, plugin, &block)
+      end
+
+      # @api private
+      def self.registry
+        @__registry__ ||= {}
+      end
+
+      # Enable a plugin
+      #
+      # Plugin identifier
+      #
+      # @param [Symbol] name The plugin identifier
+      # @param [Hash] options Plugin options
+      #
+      # @return [self]
+      #
+      # @api public
+      def use(name, options = {})
+        Plugins.registry[name].apply_to(self, options)
+        self
+      end
+    end
+  end
+end
