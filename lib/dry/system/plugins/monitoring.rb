2b23e9d2fec5726264b206ab467bd8848eeb1643
Piotr Solnica <piotr.solnica+oss@gmail.com>
2018-01-03 16:58:25 +0100

    Extract Monitoring::Proxy into a separate file

--- lib/dry/system/plugins/monitoring.rb
+++ lib/dry/system/plugins/monitoring.rb
@@ -1,51 +1,11 @@
-require 'delegate'
 require 'dry/system/constants'
-require 'dry/events/publisher'
+require 'dry/system/plugins/monitoring/proxy'
 
 module Dry
   module System
     module Plugins
       # @api public
       module Monitoring
-        class Proxy < SimpleDelegator
-          # @api private
-          def self.for(target, key:, methods: [], &block)
-            monitored_methods =
-              if methods.empty?
-                target.public_methods - Object.public_instance_methods
-              else
-                methods
-              end
-
-            Class.new(self) do
-              extend Dry::Core::ClassAttributes
-              include Dry::Events::Publisher[target.class.name]
-
-              defines :monitored_methods
-
-              attr_reader :__notifications__
-
-              monitored_methods(methods.empty? ? target.public_methods - Object.public_instance_methods : methods)
-
-              monitored_methods.each do |meth|
-                define_method(meth) do |*args, &block|
-                  object = __getobj__
-                  opts = { target: key, object: object, method: meth, args: args }
-
-                  __notifications__.instrument(:monitoring, opts) do
-                    object.public_send(meth, *args, &block)
-                  end
-                end
-              end
-            end
-          end
-
-          def initialize(target, notifications)
-            super(target)
-            @__notifications__ = notifications
-          end
-        end
-
         # @api private
         def self.extended(system)
           super
