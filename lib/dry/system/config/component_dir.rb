c25bc15a4fb6f41766cf2f46ac3391e71b89f511
Tim Riley <tim@openmonkey.com>
2021-03-16 22:50:56 +1100

    Add docs to ComponentDir and ComponentDirs

--- lib/dry/system/config/component_dir.rb
+++ lib/dry/system/config/component_dir.rb
@@ -7,20 +7,167 @@ module Dry
       class ComponentDir
         include Dry::Configurable
 
+        # @!group Settings
+
+        # @!method auto_register=(policy)
+        #
+        #   Sets the auto-registration policy for the component dir.
+        #
+        #   This may be a simple boolean to enable or disable auto-registration for all
+        #   components, or a proc accepting a `Dry::Sytem::Component` and returning a
+        #   boolean to configure auto-registration on a per-component basis
+        #
+        #   Defaults to `true`.
+        #
+        #   @param policy [Boolean, Proc]
+        #   @return [Boolean, Proc]
+        #
+        #   @example
+        #     dir.auto_register = false
+        #
+        #   @example
+        #     dir.auto_register = proc do |component|
+        #       !component.start_with?("entities")
+        #     end
+        #
+        #   @see auto_register
+        #   @see Component
+        #
+        # @!method auto_register
+        #
+        #   Returns the configured auto-registration policy.
+        #
+        #   @return [Boolean, Proc] the configured policy
+        #
+        #   @see auto_register=
         setting :auto_register, true
+
+        # @!method add_to_load_path=(policy)
+        #
+        #   Sets whether the dir should be added to the `$LOAD_PATH` after the container
+        #   is configured.
+        #
+        #   Defaults to `true`. This may need to be set to `false` when using a class
+        #   autoloading system.
+        #
+        #   @param policy [Boolean]
+        #   @return [Boolean]
+        #
+        #   @see add_to_load_path
+        #   @see Container.configure
+        #
+        # @!method add_to_load_path
+        #
+        #   Returns the configured value.
+        #
+        #   @return [Boolean]
+        #
+        #   @see add_to_load_path=
         setting :add_to_load_path, true
+
+        # @!method default_namespace=(leading_namespace)
+        #
+        #   Sets the leading namespace segments to be stripped when registering components
+        #   from the dir in the container.
+        #
+        #   This is useful to configure when the dir contains components in a module
+        #   namespace that you don't want repeated in their identifiers.
+        #
+        #   Defaults to `nil`.
+        #
+        #   @param leading_namespace [String, nil]
+        #   @return [String, nil]
+        #
+        #   @example
+        #     dir.default_namespace = "my_app"
+        #
+        #   @example
+        #     dir.default_namespace = "my_app.admin"
+        #
+        #   @see default_namespace
+        #
+        # @!method default_namespace
+        #
+        #   Returns the configured value.
+        #
+        #   @return [String, nil]
+        #
+        #   @see default_namespace=
         setting :default_namespace
+
+        # @!method loader=(loader)
+        #
+        #   Sets the loader to use when registering coponents from the dir in the container.
+        #
+        #   Defaults to `Dry::System::Loader`.
+        #
+        #   When using a class autoloader, consider using `Dry::System::Loader::Autoloading`
+        #
+        #   @param loader [#call] the loader
+        #   @return [#call] the configured loader
+        #
+        #   @see loader
+        #   @see Loader
+        #   @see Loader::Autoloading
+        #
+        # @!method loader
+        #
+        #   Returns the configured loader.
+        #
+        #   @return [#call]
+        #
+        #   @see loader=
         setting :loader, Dry::System::Loader
+
+        # @!method memoize=(policy)
+        #
+        #   Sets whether to memoize components from the dir when registered in the
+        #   container.
+        #
+        #   This may be a simple boolean to enable or disable memoization for all
+        #   components, or a proc accepting a `Dry::Sytem::Component` and returning a
+        #   boolean to configure memoization on a per-component basis
+        #
+        #   Defaults to `false`.
+        #
+        #   @param policy [Boolean, Proc]
+        #   @return [Boolean, Proc] the configured memoization policy
+        #
+        #   @example
+        #     dir.memoize = true
+        #
+        #   @example
+        #     dir.memoize = proc do |component|
+        #       !component.start_with?("providers")
+        #     end
+        #
+        #   @see memoize
+        #   @see Component
+        #
+        # @!method memoize
+        #
+        #   Returns the configured memoization policy.
+        #
+        #   @return [Boolean, Proc] the configured memoization policy
+        #
+        #   @see memoize=
         setting :memoize, false
 
+        # @!endgroup
+
+        # Returns the component dir path, relative to the configured container root
+        #
+        # @return [String] the path
         attr_reader :path
 
+        # @api private
         def initialize(path)
           super()
           @path = path
           yield self if block_given?
         end
 
+        # @api private
         def auto_register?
           !!config.auto_register
         end
--- lib/dry/system/config/component_dirs.rb
+++ lib/dry/system/config/component_dirs.rb
@@ -10,18 +10,105 @@ module Dry
       class ComponentDirs
         include Dry::Configurable
 
-        # Component dirs settings can be configured here to apply as defaults to all dirs
+        # Settings from ComponentDir can be configured here to apply all added dirs as
+        # defaults
         @_settings = ComponentDir._settings.dup
 
+        # @!group Settings
+
+        # @!method auto_register=(value)
+        #
+        #   Sets a default `auto_register` for all added component dirs
+        #
+        #   @see ComponentDir.auto_register
+        #   @see auto_register
+        #
+        # @!method auto_register
+        #
+        #   Returns the configured default `auto_register`
+        #
+        #   @see auto_register=
+
+        # @!method add_to_load_path=(value)
+        #
+        #   Sets a default `add_to_load_path` value for all added component dirs
+        #
+        #   @see ComponentDir.add_to_load_path
+        #   @see add_to_load_path
+        #
+        # @!method add_to_load_path
+        #
+        #   Returns the configured default `add_to_load_path`
+        #
+        #   @see add_to_load_path=
+
+        # @!method default_namespace=(value)
+        #
+        #   Sets a default `default_namespace` value for all added component dirs
+        #
+        #   @see ComponentDir.default_namespace
+        #   @see default_namespace
+        #
+        # @!method default_namespace
+        #
+        #   Returns the configured default `default_namespace`
+        #
+        #   @see default_namespace=
+
+        # @!method loader=(value)
+        #
+        #   Sets a default `loader` value for all added component dirs
+        #
+        #   @see ComponentDir.loader
+        #   @see loader
+        #
+        # @!method loader
+        #
+        #   Returns the configured default `loader`
+        #
+        #   @see loader=
+
+        # @!method memoize=(value)
+        #
+        #   Sets a default `memoize` value for all added component dirs
+        #
+        #   @see ComponentDir.memoize
+        #   @see memoize
+        #
+        # @!method memoize
+        #
+        #   Returns the configured default `memoize`
+        #
+        #   @see memoize=
+
+        # @!endgroup
+
+        # @api private
         def initialize
           @dirs = Concurrent::Map.new
         end
 
+        # @api private
         def initialize_copy(source)
           super
           @dirs = source.dirs.dup
         end
 
+        # Adds and configures a component dir
+        #
+        # @param path [String] the path for the component dir, relative to the configured
+        #   container root
+        #
+        # @yieldparam dir [ComponentDir] the component dir to configure
+        #
+        # @return [ComponentDir] the added component dir
+        #
+        # @example
+        #   component_dirs.add "lib" do |dir|
+        #     dir.default_namespace = "my_app"
+        #   end
+        #
+        # @see ComponentDir
         def add(path)
           raise ComponentDirAlreadyAddedError, path if dirs.key?(path)
 
@@ -30,14 +117,24 @@ module Dry
           end
         end
 
+        # Returns the added component dirs, with default settings applied
+        #
+        # @return [Hash<String, ComponentDir>] the component dirs as a hash, keyed by path
         def dirs
           @dirs.each { |_, dir| apply_defaults_to_dir(dir) }
         end
 
+        # Returns the added component dirs, with default settings applied
+        #
+        # @return [Array<ComponentDir>]
         def to_a
           dirs.values
         end
 
+        # Calls the given block once for each added component dir, passing the dir as an
+        # argument.
+        #
+        # @yieldparam dir [ComponentDir] the yielded component dir
         def each(&block)
           to_a.each(&block)
         end
@@ -45,7 +142,8 @@ module Dry
         private
 
         # Apply default settings to a component dir. This is run every time the dirs are
-        # accessed, so this must be idempotent
+        # accessed to ensure defaults are applied regardless of when new component dirs
+        # are added. This method must be idempotent.
         #
         # @return [void]
         def apply_defaults_to_dir(dir)
