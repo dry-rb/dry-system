db5b3a2fb4650095aff656cea33fe5053808850d
wuarmin <armin@wurzweb.com>
2021-09-11 10:59:47 +0200

    CHG: fix standalone example

--- examples/standalone/lib/user_repo.rb
+++ examples/standalone/lib/user_repo.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'import'
-
 class UserRepo
-  include Import['persistence.db']
+  include Import["persistence.db"]
 end
--- examples/standalone/run.rb
+++ examples/standalone/run.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
-require_relative 'system/container'
-require 'dry/events'
-require 'dry/monitor/notifications'
+require "bundler/setup"
+require_relative "system/container"
+require_relative "system/import"
+require "dry/events"
+require "dry/monitor/notifications"
 
 App[:notifications].subscribe(:resolved_dependency) do |event|
   puts "Event #{event.id}, payload: #{event.to_h}"
@@ -16,7 +17,7 @@ end
 App.finalize!
 p App.keys
 
-App['service_with_dependency']
-user_repo = App['user_repo']
+App["service_with_dependency"]
+user_repo = App["user_repo"]
 
 puts user_repo.db.inspect
