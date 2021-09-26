25d027c80a9e9eb33ec0593cf401e3895ab63175
Tim Riley <tim@openmonkey.com>
2021-09-26 23:20:33 +1000

    Reboot RSpec setup
    Use the standard RSpec-generated spec_helper file (shifted into spec/support/rspec.rb to keep the base spec_helper.rb clearer)
    
    Expand upon the support for loaded constants cleaning and move it into its own file in spec/support/.
    
    Add a new spec/support/tmp_directory.rb providing helpers for generating fixture files for individual specs, rather than relying on a far-removed and hard-to-understand set of fixed files in spec/fixtures/ that need to serve multiple purposes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,8 @@
 
 require "bundler/setup"
 
-require_relative "support/coverage"
-
 require "pathname"
-
 require "warning"
-# Warning.process { |w| raise RuntimeError, w } unless ENV['NO_WARNING']
 
 begin
   require "byebug"
@@ -15,43 +11,20 @@ begin
 rescue LoadError; end
 SPEC_ROOT = Pathname(__FILE__).dirname
 
-Dir[SPEC_ROOT.join("support/*.rb").to_s].each { |f| require f }
-Dir[SPEC_ROOT.join("shared/*.rb").to_s].each { |f| require f }
+Dir[SPEC_ROOT.join("support/*.rb").to_s].sort.each { |f| require f }
+Dir[SPEC_ROOT.join("shared/*.rb").to_s].sort.each { |f| require f }
 
 require "dry/system/container"
 require "dry/system/stubs"
 require "dry/events"
 
-module TestNamespace
-  def remove_constants
-    constants.each do |name|
-      remove_const(name)
-    end
-  end
-end
-
-# for specs that rely on `settings` DSL
+# For specs that rely on `settings` DSL
 module Types
   include Dry::Types()
 end
 
 RSpec.configure do |config|
-  config.disable_monkey_patching!
-
-  config.before do
-    @load_paths = $LOAD_PATH.dup
-    @loaded_features = $LOADED_FEATURES.dup
-    Object.const_set(:Test, Module.new { |m| m.extend(TestNamespace) })
-  end
-
   config.after do
-    $LOAD_PATH.replace(@load_paths)
-    $LOADED_FEATURES.replace(@loaded_features)
-
-    Test.remove_constants
-    Object.send(:remove_const, :Test)
-    Object.send(:remove_const, :Namespaced) if defined? Namespaced
-
     Dry::System.providers.items.delete_if { |p| p.identifier != :system }
   end
 end
---
+++ spec/support/tmp_directory.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "fileutils"
+require "pathname"
+require "tmpdir"
+
+module RSpec
+  module Support
+    module TmpDirectory
+      private
+
+      def with_tmp_directory(&block)
+        Dir.mktmpdir(&block)
+      end
+
+      def with_directory(dir, &block)
+        Dir.chdir(dir, &block)
+      end
+
+      def make_tmp_directory
+        Pathname(Dir.mktmpdir).tap do |dir|
+          (@made_tmp_dirs ||= []) << dir
+        end
+      end
+
+      def write(path, *content)
+        Pathname.new(path).dirname.mkpath
+
+        File.open(path, ::File::CREAT | ::File::WRONLY) do |file|
+          file.write(Array(content).flatten.join)
+        end
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpec::Support::TmpDirectory
+
+  config.after :all do
+    if instance_variable_defined?(:@made_tmp_dirs)
+      Array(@made_tmp_dirs).each do |dir|
+        FileUtils.remove_entry dir
+      end
+    end
+  end
+end
