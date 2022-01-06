fd33b4c722be66eb135d8607cd7b01759f2dac25
Tim Riley <tim@openmonkey.com>
2022-01-06 16:52:09 +1100

    Fix API docs

--- lib/dry/system.rb
+++ lib/dry/system.rb
@@ -5,9 +5,7 @@ require_relative "system/source_provider_registry"
 
 module Dry
   module System
-    # FIXME: update docs
-
-    # Register external component provider
+    # Registers all the source providers in the files under the given path
     #
     # @api public
     def self.register_source_providers(path)
@@ -25,6 +23,7 @@ module Dry
       register_source_providers(options.fetch(:path))
     end
 
+    # Registers a source provider, which can be used as the basis for other providers
     #
     # @api public
     def self.register_source_provider(name, group:, &block)
