669a0b8c0090683458dfd2525f30f3f19dddd24c
Tim Riley <tim@openmonkey.com>
2022-01-16 14:58:22 +1100

    Tidy the dependency graph plugin
    This makes some changes to make the dependency graph plugin less intrusive on the overall container behaviour. It:
    
    - Nests its setting underneath a top-level `dependency_graph` setting
    - Removes the ambiguously named and potentially buggy (due to its ivars that may end up being set after the cotnainer is frozen) `Container.strategies` method and instead provides its custom auto-injector strategies via an overload of the container's public `.injector` method options.

--- lib/dry/system/plugins/dependency_graph.rb
+++ lib/dry/system/plugins/dependency_graph.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-require "dry/system/constants"
-require "dry/system/plugins/dependency_graph/strategies"
+require_relative "dependency_graph/strategies"
 
 module Dry
   module System
@@ -12,34 +11,43 @@ module Dry
         def self.extended(system)
           super
 
-          system.use(:notifications)
+          system.instance_eval do
+            use(:notifications)
 
-          system.setting :ignored_dependencies, default: []
+            setting :dependency_graph do
+              setting :ignored_dependencies, default: []
+            end
 
-          system.after(:configure) do
-            self[:notifications].register_event(:resolved_dependency)
-            self[:notifications].register_event(:registered_dependency)
-
-            strategies(Strategies)
+            after(:configure) do
+              self[:notifications].register_event(:resolved_dependency)
+              self[:notifications].register_event(:registered_dependency)
+            end
           end
         end
 
         # @api private
         def self.dependencies
-          {'dry-events': "dry/events/publisher"}
+          {"dry-events" => "dry/events/publisher"}
+        end
+
+        # @api private
+        def injector(**options)
+          super(**options.merge(strategies: DependencyGraph::Strategies))
         end
 
         # @api private
         def register(key, contents = nil, options = {}, &block)
-          super
-          dependency_key = key.to_s
-          unless config.ignored_dependencies.include?(dependency_key)
-            self[:notifications].instrument(
-              :registered_dependency, key: dependency_key, class: self[dependency_key].class
-            )
-          end
+          super.tap do
+            key = key.to_s
 
-          self
+            unless config.dependency_graph.ignored_dependencies.include?(key)
+              self[:notifications].instrument(
+                :registered_dependency,
+                key: key,
+                class: self[key].class
+              )
+            end
+          end
         end
       end
     end
