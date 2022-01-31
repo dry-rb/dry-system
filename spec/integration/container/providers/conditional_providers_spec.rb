dbe5562df228508c76dd3bd5f52ae424f2da8e7f
Tim Riley <tim@openmonkey.com>
2022-01-31 13:36:24 +1100

    Expand specs for file-based providers

--- spec/integration/container/providers/conditional_providers_spec.rb
+++ spec/integration/container/providers/conditional_providers_spec.rb
@@ -13,22 +13,16 @@ RSpec.describe "Providers / Conditional providers" do
   }
 
   shared_examples "loads the provider" do
-    it "registers the provider" do
-      expect(container.providers.key?(:provided)).to be true
-    end
-
-    it "runs the provider" do
+    it "runs the provider when a related component is resolved" do
       expect(container["provided"]).to be
+      expect(container.providers.key?(:provided)).to be true
     end
   end
 
   shared_examples "does not load the provider" do
-    it "does not register the provider" do
-      expect(container.providers.key?(:provided)).to be false
-    end
-
-    it "does not run the provider" do
+    it "does not run the provider when a related component is resolved" do
       expect { container["provided"] }.to raise_error(Dry::Container::Error, /Nothing registered/)
+      expect(container.providers.key?(:provided)).to be false
     end
   end
 
@@ -55,6 +49,63 @@ RSpec.describe "Providers / Conditional providers" do
     context "finalized" do
       before { container.finalize! }
       include_examples "does not load the provider"
+    end
+  end
+
+  describe "provider file in provider dir" do
+    let(:container) {
+      root = @dir
+      Test::Container = Class.new(Dry::System::Container) {
+        configure do |config|
+          config.root = root
+        end
+      }
+    }
+
+    describe "true" do
+      before :context do
+        with_directory(@dir = make_tmp_directory) do
+          write "system/providers/provided.rb", <<~RUBY
+            Test::Container.register_provider :provided, if: true do
+              start do
+                register "provided", Object.new
+              end
+            end
+          RUBY
+        end
+      end
+
+      context "lazy loading" do
+        include_examples "loads the provider"
+      end
+
+      context "finalized" do
+        before { container.finalize! }
+        include_examples "loads the provider"
+      end
+    end
+
+    describe "true" do
+      before :context do
+        with_directory(@dir = make_tmp_directory) do
+          write "system/providers/provided.rb", <<~RUBY
+            Test::Container.register_provider :provided, if: false do
+              start do
+                register "provided", Object.new
+              end
+            end
+          RUBY
+        end
+      end
+
+      context "lazy loading" do
+        include_examples "does not load the provider"
+      end
+
+      context "finalized" do
+        before { container.finalize! }
+        include_examples "does not load the provider"
+      end
     end
   end
 end
