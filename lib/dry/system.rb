29c82b3fda2cfd393ab05138e2ae60b017e80878
Peter Solnica <peter@solnica.online>
2022-10-14 15:42:33 +0200

    Add zeitwerk loader

--- lib/dry/system.rb
+++ lib/dry/system.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
+require "zeitwerk"
 require "dry/core"
 require_relative "system/provider_source_registry"
 
 module Dry
   module System
+    # @api private
+    def self.loader
+      @loader ||= Zeitwerk::Loader.new.tap do |loader|
+        root = File.expand_path("..", __dir__)
+        loader.tag = "dry-system"
+        loader.inflector = Zeitwerk::GemInflector.new("#{root}/dry-system.rb")
+        loader.push_dir(root)
+        loader.ignore(
+          "#{root}/dry-system.rb",
+          "#{root}/dry/system/{constants,errors,stubs,version}.rb"
+        )
+        loader.inflector.inflect("dsl" => "DSL")
+      end
+    end
+
     # Registers the provider sources in the files under the given path
     #
     # @api public
@@ -58,5 +74,7 @@ module Dry
     def self.provider_sources
       @provider_sources ||= ProviderSourceRegistry.new
     end
+
+    loader.setup
   end
 end
