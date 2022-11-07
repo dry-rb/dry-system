7e77493a67c046fb762747fee1708ec525fbc1e7
Xavier Noria <fxn@hashref.com>
2022-11-07 13:48:07 +0100

    Remove usage of the private Zeitwerk::Registry.loaders (#260)

--- spec/support/zeitwerk_helpers.rb
+++ spec/support/zeitwerk_helpers.rb
@@ -2,17 +2,9 @@
 
 module ZeitwerkHelpers
   def teardown_zeitwerk
-    # From zeitwerk's own test/support/loader_test
-    # adjusted to work with dry-rb gem loaders
-
-    Zeitwerk::Registry.loaders.reject! do |loader|
-      test_loader = loader.dirs.any? { |dir| dir.include?("/spec/") || dir.include?(Dir.tmpdir) }
-
-      if test_loader
+    ObjectSpace.each_object(Zeitwerk::Loader) do |loader|
+      if loader.dirs.any? { |dir| dir.include?("/spec/") || dir.include?(Dir.tmpdir) }
         loader.unregister
-        true
-      else
-        false
       end
     end
   end
