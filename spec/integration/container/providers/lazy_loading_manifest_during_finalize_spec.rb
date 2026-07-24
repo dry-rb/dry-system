dry-rb/dry-system

commit 9757fc4b688299e71051934c5072ec17b488f116

Use load instead of require for ManifestRegistrar (#286)
This is a bug that is caused by the interaction between dry-system and
dry-rails.

Dry::Rails rebuilds the Container from scratch on every reload, which
resets all the keys. Since ManifestRegistrar was using `require`, this
reload would not register the keys as expected.

lib/dry/system/manifest_registrar.rb
@@ -33,7 +33,7 @@ module Dry
 
       # @api private
       def call(component)
-        require(root.join(config.registrations_dir, component.root_key.to_s))
+        load(root.join(config.registrations_dir, "#{component.root_key}#{RB_EXT}"))
       end
 
       # @api private

spec/integration/container/lazy_loading/manifest_registration_spec.rb
@@ -1,19 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe "Lazy-loading registration manifest files" do
-  before do
-    module Test
-      class Container < Dry::System::Container
-        configure do |config|
-          config.root = SPEC_ROOT.join("fixtures/manifest_registration").realpath
-        end
+  module Test; end
 
-        add_to_load_path!("lib")
+  def build_container
+    Class.new(Dry::System::Container) do
+      configure do |config|
+        config.root = SPEC_ROOT.join("fixtures/manifest_registration").realpath
       end
     end
   end
 
   shared_examples "manifest component" do
+    before do
+      Test::Container = build_container
+      Test::Container.add_to_load_path!("lib")
+    end
+
     it "loads a registration manifest file if the component could not be found" do
       expect(Test::Container["foo.special"]).to be_a(Test::Foo)
       expect(Test::Container["foo.special"].name).to eq "special"
@@ -25,7 +28,36 @@ RSpec.describe "Lazy-loading registration manifest files" do
   end
 
   context "Finalized container" do
-    before { Test::Container.finalize! }
     include_examples "manifest component"
+    before { Test::Container.finalize! }
+  end
+
+  context "Autoloaded container" do
+    let :autoloader do
+      Zeitwerk::Loader.new.tap do |loader|
+        loader.enable_reloading
+
+        # This is a simulacrum of the Dry::Rails container reset
+        # that happens on every reload
+        loader.on_setup do
+          if Test.const_defined?(:Container)
+            Test.__send__(:remove_const, :Container)
+          end
+
+          Test.const_set :Container, build_container
+          Test::Container.finalize!
+
+          loader.push_dir(Test::Container.root)
+        end
+      end
+    end
+
+    it "reloads manifest keys" do
+      autoloader.setup
+      expect(Test::Container.keys).to include("foo.special")
+
+      autoloader.reload
+      expect(Test::Container.keys).to include("foo.special")
+    end
   end
 end

commit 9bbe32e46fdd4881bc2e5ef5065a05831eaa9074

ManifestRegistrar#finalize! avoids duplicates (#295)
Fix a small bug introduced by #286. When a container is finalizing, providers are finalized first. If one of those providers loads a manifest-registered dependency, that manifest will already have been loaded by the time the manifest registrar finalizes. Keep track of loaded manifest files to avoid attempting to load them a second time, which would result in errors around duplicate registrations.

---------

Co-authored-by: Tim Riley <tim@riley.id.au>

lib/dry/system/manifest_registrar.rb
@@ -22,17 +22,24 @@ module Dry
       def initialize(container)
         @container = container
         @config = container.config
+        @loaded = Set.new
       end
 
       # @api private
       def finalize!
         ::Dir[registrations_dir.join(RB_GLOB)].each do |file|
-          call(Identifier.new(File.basename(file, RB_EXT)))
+          ident = Identifier.new(File.basename(file, RB_EXT))
+
+          # Skip files already loaded during earlier stages of finalization, such as a provider
+          # resolving a component satisfied via a manifest. Since `#call` uses `load`, re-running
+          # here would attempt a duplicate registration and raise an error.
+          call(ident) unless @loaded.include?(ident.root_key)
         end
       end
 
       # @api private
       def call(component)
+        @loaded << component.root_key
         load(root.join(config.registrations_dir, "#{component.root_key}#{RB_EXT}"))
       end
 

spec/integration/container/providers/lazy_loading_manifest_during_finalize_spec.rb
@@ -1,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe "Providers / Lazy loading a manifest-registered dependency as part of finalize" do
+  before :context do
+    @dir = make_tmp_directory
+
+    with_directory(@dir) do
+      write "system/registrations/deps.rb", <<~RUBY
+        Test::Container.namespace(:deps) do |container|
+          container.register(:thing) { Object.new }
+        end
+      RUBY
+    end
+  end
+
+  before do
+    root = @dir
+    Test::Container = Class.new(Dry::System::Container) do
+      configure do |config|
+        config.root = root
+      end
+
+      register_provider :my_provider do
+        start do
+          target["deps.thing"]
+        end
+      end
+    end
+  end
+
+  it "does not register the manifest dependency more than once" do
+    expect { Test::Container.finalize! }.not_to raise_error
+    expect(Test::Container["deps.thing"]).to be
+  end
+end