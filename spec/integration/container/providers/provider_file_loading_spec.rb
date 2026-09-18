b2d0a32fca20e1d5eea1cdafad8892bca1f83352
Tim Riley <tim@riley.id.au>
2026-09-18 16:26:23 +1000

    Rejig test to avoid global accessors

--- spec/integration/container/providers/provider_file_loading_spec.rb
+++ spec/integration/container/providers/provider_file_loading_spec.rb
@@ -2,76 +2,69 @@
 
 RSpec.describe "Providers / Loading provider files" do
   before do
-    module Test
-      class << self
-        attr_accessor :container, :message
-      end
-    end
-
-    Test.message = "hello"
-
     @dir = make_tmp_directory
 
     write_provider <<~RUBY
-      Test.container.register_provider(:greeter) do
-        start { register(:greeter, Test.message) }
+      Test::Container.register_provider(:greeter) do
+        start { register(:greeter, "hello") }
       end
     RUBY
   end
 
   def write_provider(content)
-    dir = File.join(@dir, "system", "providers")
-    FileUtils.mkdir_p(dir)
-
-    File.write(File.join(dir, "greeter.rb"), content)
-  end
-
-  def build_container
-    root = @dir
-
-    Class.new(Dry::System::Container) do
-      config.root = root
+    with_directory(@dir) do
+      write "system/providers/greeter.rb", content
     end
   end
 
-  specify "a provider file is evaluated once per container, not once per process" do
-    Test.container = build_container
-    expect(Test.container[:greeter]).to eq "hello"
+  # Mimics a container being unloaded and then reloaded, as a code reloader does: the container
+  # class is discarded, then defined again under the same name.
+  def load_container
+    Test.send(:remove_const, :Container) if Test.const_defined?(:Container, false)
 
-    # A second container sharing the provider dir must get its own provider, rather than having
-    # the file skipped because the first container already loaded it.
-    Test.container = build_container
-    expect(Test.container[:greeter]).to eq "hello"
+    root = @dir
+
+    container = Class.new(Dry::System::Container) do
+      config.root = root
+    end
+
+    Test.const_set(:Container, container)
   end
 
-  specify "a container built afresh picks up changes to a provider file" do
-    Test.container = build_container
-    expect(Test.container[:greeter]).to eq "hello"
+  specify "a reloaded container registers its providers again" do
+    expect(load_container[:greeter]).to eq "hello"
+
+    # The provider file is evaluated once per container, rather than once per process, so a new
+    # container still gets this provider.
+    expect(load_container[:greeter]).to eq "hello"
+  end
+
+  specify "a reloaded container picks up changes to a provider file" do
+    expect(load_container[:greeter]).to eq "hello"
 
     write_provider <<~RUBY
-      Test.container.register_provider(:greeter) do
-        start { register(:greeter, Test.message.upcase) }
+      Test::Container.register_provider(:greeter) do
+        start { register(:greeter, "HELLO") }
       end
     RUBY
 
-    Test.container = build_container
-    expect(Test.container[:greeter]).to eq "HELLO"
+    expect(load_container[:greeter]).to eq "HELLO"
   end
 
   specify "a provider file is evaluated at most once for a given container" do
     # The provider name does not match the file name, so looking up `:greeter` loads the file
     # without ever finding the provider it was looking for.
     write_provider <<~RUBY
-      Test.container.register_provider(:other_name) do
-        start { register(:greeter, Test.message) }
+      Test::Container.register_provider(:other_name) do
+        start { register(:greeter, "hello") }
       end
     RUBY
 
-    Test.container = build_container
+    container = load_container
 
-    expect(Test.container.providers[:greeter]).to be_nil
+    expect(container.providers[:greeter]).to be_nil
 
     # Loading the file a second time would re-register `:other_name` and raise.
-    expect { Test.container.providers[:greeter] }.not_to raise_error
+    expect { container.providers[:greeter] }.not_to raise_error
   end
 end
